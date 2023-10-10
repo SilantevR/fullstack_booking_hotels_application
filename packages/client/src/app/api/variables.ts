a5d561c4914e951cf24aeb655f458a9abc6f3657
craft <craft_89@inbox.ru>
2023-10-10 20:48:51 +0300

    update README

--- packages/client/src/app/api/variables.ts
+++ packages/client/src/app/api/variables.ts
@@ -3,7 +3,7 @@ import { Options } from "./types";
 const BASE_URL =
   `${import.meta.env.VITE_BASE_URL}` || "http://localhost:5000/api";
 export const PUBLIC_URL =
-  `${import.meta.env.VITE_PUBLIC_URL}` || "http://localhost:5000/";
+  `${import.meta.env.VITE_PUBLIC_URL}` || "http://localhost:5000";
 const baseOptions: Options = {
   credentials: "include",
   headers: {
