d9a68ea10b0901022e93d6e1fded06619ced32d2
craft <craft_89@inbox.ru>
2023-11-18 15:04:02 +0300

    deploy change

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,14 @@
 import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: config.get('SOKET_ORIGIN'),
     credentials: true,
   });
   await app.listen(5000, '127.0.0.1');
